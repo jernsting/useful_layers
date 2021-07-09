de21c0fe2750e26e7ef00988a3700d868cda6be5
Jan Ernsting <j.ernsting@uni-muenster.de>
2021-07-10 00:13:28 +0200

    Minor change of setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(
     version=__version__,
     description="""
 Useful Layers
-Useful Layers is a torch based library containing some experimental, 
+Useful Layers is a torch based library containing some experimental,
 but useful layers
 """,
     author='Jan Ernsting',
     author_email='j.ernsting@uni-muenster.de',
     url='https://github.com/jernsting/useful_layers.git',
-    download_url='https://github.com/jernsting/useful_layers/archive/' + 
+    download_url='https://github.com/jernsting/useful_layers/archive/' +
     __version__ + '.tar.gz',
     keywords=['machine learning', 'deep learning', 'experimental', 'science'],
     classifiers=[],
