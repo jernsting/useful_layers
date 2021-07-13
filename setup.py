867c6b43448f6ee8f9f9f7e2289afc6119244ea4
Jan Ernsting <j.ernsting@uni-muenster.de>
2021-07-14 00:14:08 +0200

    prepared first release

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-__version__ = '0.0.1'
+__version__ = '0.0a0.dev1'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='useful_layers',
@@ -13,15 +16,28 @@ setup(
     include_package_data=True,
     version=__version__,
     description="""
-Useful Layers
 Useful Layers is a torch based library containing some experimental,
 but useful layers
 """,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jan Ernsting',
     author_email='j.ernsting@uni-muenster.de',
-    url='https://github.com/jernsting/useful_layers.git',
+    url='https://jernsting.github.io/useful_layers/',
     download_url='https://github.com/jernsting/useful_layers/archive/' +
     __version__ + '.tar.gz',
     keywords=['machine learning', 'deep learning', 'experimental', 'science'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    project_urls={
+        "Source Code": "https://github.com/jernsting/useful_layers",
+        "Documentation": 'https://jernsting.github.io/useful_layers/',
+        "Bug Tracker": "https://github.com/jernsting/useful_layers/issues"
+    }
 )
